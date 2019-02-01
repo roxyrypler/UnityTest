e2ebdb85e307cf4bac390d198f88107e849cec7b
Patrick Lie <patrick.lie9@gmail.com>
2019-02-01 14:38:06 +0100

    Added new dynamic drag n drop script

---
+++ Assets/Scripts/DragAndDropDynamicScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDropDynamicScript : MonoBehaviour
+{
+
+    private Vector2 prewPos;
+    bool isDragging = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prewPos = new Vector2(gameObject.transform.position.x, gameObject.transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        bool mouseIsOver = gameObject.GetComponent<SpriteRenderer>().bounds.Contains(mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (mouseIsOver)
+            {
+                isDragging = true;
+            }
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            isDragging = false;
+        }
+
+        dragging(isDragging, mousePosition);
+
+
+    }
+
+    void dragging(bool isDragging, Vector2 mousePos)
+    {
+        if (isDragging)
+        {
+            gameObject.transform.position = new Vector2(mousePos.x, mousePos.y);
+        }
+    }
+}
